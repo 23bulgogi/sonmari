c9d5f353d7088155395baf8c52e757dda6845733
whogodemo <anabel604@ewhain.net>
2020-12-10 17:45:34 +0900

    Add files via upload

---
+++ cnn_tf.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import numpy as np
+import pickle, os, cv2
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+def get_image_size():
+	img = cv2.imread('dist\\gestures\\0\\100.jpg', 0)
+	return img.shape
+
+def get_num_of_classes():
+	return len(os.listdir('dist\\gestures/'))
+
+image_x, image_y = get_image_size()
+
+def cnn_model_fn(features, labels, mode):
+	input_layer = tf.reshape(features["x"], [-1, image_x, image_y, 1], name="input")
+
+	conv1 = tf.layers.conv2d(
+	  inputs=input_layer,
+	  filters=16,
+	  kernel_size=[2, 2],
+	  padding="same",
+	  activation=tf.nn.relu,
+	  name="conv1")
+	print("conv1",conv1.shape)
+	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2, name="pool1")
+	print("pool1",pool1.shape)
+
+	conv2 = tf.layers.conv2d(
+	  inputs=pool1,
+	  filters=32,
+	  kernel_size=[5, 5],
+	  padding="same",
+	  activation=tf.nn.relu,
+	  name="conv2")
+	print("conv2",conv2.shape)
+	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[5, 5], strides=5, name="pool2")
+	print("pool2",pool2.shape)
+
+	conv3 = tf.layers.conv2d(
+	  inputs=pool2,
+	  filters=64,
+	  kernel_size=[5, 5],
+	  padding="same",
+	  activation=tf.nn.relu,
+	  name="conv3")
+	print("conv3",conv3.shape)
+
+	# Dense Layer
+	flat = tf.reshape(conv3, [-1, 5*5*64], name="flat")
+	print(flat.shape)
+	dense = tf.layers.dense(inputs=flat, units=128, activation=tf.nn.relu, name="dense")
+	print(dense.shape)
+	dropout = tf.layers.dropout(inputs=dense, rate=0.2, training=mode == tf.estimator.ModeKeys.TRAIN, name="dropout")
+
+	# Logits Layer
+	num_of_classes = get_num_of_classes()
+	logits = tf.layers.dense(inputs=dropout, units=num_of_classes, name="logits")
+
+	output_class = tf.argmax(input=logits, axis=1, name="output_class")
+	output_probab = tf.nn.softmax(logits, name="softmax_tensor")
+	predictions = {"classes": tf.argmax(input=logits, axis=1), "probabilities": tf.nn.softmax(logits, name="softmax_tensor")}
+	#tf.Print(tf.nn.softmax(logits, name="softmax_tensor"), [tf.nn.softmax(logits, name="softmax_tensor")])
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+	# Calculate Loss (for both TRAIN and EVAL modes)
+	onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=num_of_classes)
+	loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+
+	# Configure the Training Op (for TRAIN mode)
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2)
+		train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+		return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+	# Add evaluation metrics (for EVAL mode)
+	eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
+	return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(argv):
+	with open("train_images", "rb") as f:
+		train_images = np.array(pickle.load(f))
+	with open("train_labels", "rb") as f:
+		train_labels = np.array(pickle.load(f), dtype=np.int32)
+
+	with open("test_images", "rb") as f:
+		test_images = np.array(pickle.load(f))
+	with open("test_labels", "rb") as f:
+		test_labels = np.array(pickle.load(f), dtype=np.int32)
+	#print(len(train_images[1]), len(train_labels))
+
+	classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="tmp/cnn_model3")
+
+	tensors_to_log = {"probabilities": "softmax_tensor"}
+	logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
+
+	train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": train_images}, y=train_labels, batch_size=500, num_epochs=10, shuffle=True)
+	classifier.train(input_fn=train_input_fn, hooks=[logging_hook])
+
+	# Evaluate the model and print results
+	eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+	  x={"x": test_images},
+	  y=test_labels,
+	  num_epochs=1,
+	  shuffle=False)
+	test_results = classifier.evaluate(input_fn=eval_input_fn)
+	print(test_results)
+
+
+if __name__ == "__main__":
+	tf.app.run()
---
+++ recognize_gesture.py
@@ -0,0 +1,118 @@
+import cv2, pickle
+import numpy as np
+import tensorflow as tf
+from cnn_tf import cnn_model_fn
+import os
+import sqlite3
+from keras.models import load_model
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.logging.set_verbosity(tf.logging.ERROR)
+classifier = tf.estimator.Estimator(model_dir="tmp/cnn_model2", model_fn=cnn_model_fn)
+prediction = None
+model = load_model('cnn_model_keras2.h5')
+
+def get_image_size():
+	img = cv2.imread('gestures\\0\\100.jpg', 0)
+	return img.shape
+
+image_x, image_y = get_image_size()
+
+def tf_process_image(img):
+	img = cv2.resize(img, (image_x, image_y))
+	img = np.array(img, dtype=np.float32)
+	np_array = np.array(img)
+	return np_array
+
+def tf_predict(classifier, image):
+	'''
+	need help with prediction using tensorflow
+	'''
+	global prediction
+	processed_array = tf_process_image(image)
+	pred_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x":processed_array}, shuffle=False)
+	pred = classifier.predict(input_fn=pred_input_fn)
+	prediction = next(pred)
+	print(prediction)
+
+def keras_process_image(img):
+	img = cv2.resize(img, (image_x, image_y))
+	img = np.array(img, dtype=np.float32)
+	img = np.reshape(img, (1, image_x, image_y, 1))
+	return img
+
+def keras_predict(model, image):
+	processed = keras_process_image(image)
+	pred_probab = model.predict(processed)[0]
+	pred_class = list(pred_probab).index(max(pred_probab))
+	return max(pred_probab), pred_class
+
+def get_pred_text_from_db(pred_class):
+	conn = sqlite3.connect("gesture_db.db")
+	cmd = "SELECT g_name FROM gesture WHERE g_id="+str(pred_class)
+	cursor = conn.execute(cmd)
+	for row in cursor:
+		return row[0]
+
+
+def get_hand_hist():
+	with open("hist", "rb") as f:
+		hist = pickle.load(f)
+	return hist
+
+def recognize():
+	global prediction
+	cam = cv2.VideoCapture(1)
+	if cam.read()[0] == False:
+		cam = cv2.VideoCapture(0)
+	hist = get_hand_hist()
+	x, y, w, h = 300, 100, 300, 300
+	while True:
+		text = ""
+		img = cam.read()[1]
+		img = cv2.flip(img, 1)
+		img = cv2.resize(img, (640, 480))
+		imgCrop = img[y:y+h, x:x+w]
+		imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		dst = cv2.calcBackProject([imgHSV], [0, 1], hist, [0, 180, 0, 256], 1)
+		disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
+		cv2.filter2D(dst,-1,disc,dst)
+		blur = cv2.GaussianBlur(dst, (11,11), 0)
+		blur = cv2.medianBlur(blur, 15)
+		thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+		thresh = cv2.merge((thresh,thresh,thresh))
+		thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+		thresh = thresh[y:y+h, x:x+w]
+		(openCV_ver,_,__) = cv2.__version__.split(".")
+		if openCV_ver=='3':
+			contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+		elif openCV_ver=='4':
+			contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+		if len(contours) > 0:
+			contour = max(contours, key = cv2.contourArea)
+			#print(cv2.contourArea(contour))
+			if cv2.contourArea(contour) > 10000:
+				x1, y1, w1, h1 = cv2.boundingRect(contour)
+				save_img = thresh[y1:y1+h1, x1:x1+w1]
+				
+				if w1 > h1:
+					save_img = cv2.copyMakeBorder(save_img, int((w1-h1)/2) , int((w1-h1)/2) , 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
+				elif h1 > w1:
+					save_img = cv2.copyMakeBorder(save_img, 0, 0, int((h1-w1)/2) , int((h1-w1)/2) , cv2.BORDER_CONSTANT, (0, 0, 0))
+				
+				pred_probab, pred_class = keras_predict(model, save_img)
+				
+				if pred_probab*100 > 80:
+					text = get_pred_text_from_db(pred_class)
+					print(text)
+		blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
+		cv2.putText(blackboard, text, (30, 200), cv2.FONT_HERSHEY_TRIPLEX, 1.3, (255, 255, 255))
+		cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+		res = np.hstack((img, blackboard))
+		cv2.imshow("Recognizing gesture", res)
+		cv2.imshow("thresh", thresh)
+		if cv2.waitKey(1) == ord('q'):
+			break
+
+keras_predict(model, np.zeros((50, 50), dtype=np.uint8))		
+recognize()
