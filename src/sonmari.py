2a9696f6597a7b6f3b93d589c7454f02465b5209
Jeongkyu Shin <inureyes@gmail.com>
2021-10-21 14:25:42 +0900

    update: change model directory from backup to model (#8)

--- src/sonmari.py
+++ src/sonmari.py
@@ -19,7 +19,7 @@ def parser():
                         help="video source. If empty, uses webcam 0 stream")
     parser.add_argument("--out_filename", type=str, default="",
                         help="inference video name. Not saved if empty")
-    parser.add_argument("--weights", default="./backup/yolov4-obj_96_best.weights",
+    parser.add_argument("--weights", default="./model/yolov4-obj_96_best.weights",
                         help="yolo weights path")
     parser.add_argument("--dont_show", action='store_true',
                         help="windown inference display. For headless systems")
